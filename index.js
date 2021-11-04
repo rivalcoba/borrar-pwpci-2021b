9de13bac35aa45f7290b5df8577e231213ed47f2
rivalcoba <ivan.rivalcoba@gmail.com>
2021-11-04 08:34:41 -0600

    S23 Sirviendo HTML

--- index.js
+++ index.js
@@ -6,14 +6,23 @@ import http from "http";
 // ante cualquier peticion de un recurso a nuestro server
 // (request, response)
 const server = http.createServer((req, res) => {
+  // Informa en la consola del servidor que se recibe una petición
   console.log("> Se ha recibido una petición.");
+
   // Registrar información de la petición
   console.log(`✒ ✒ Información de la petición`);
-  console.log(`✒ url: ${req.url}`);
+  console.log(`✒ Url: ${req.url}`);
   console.log(`✒ Request Method: ${req.method}`);
-  console.log(`✒ Plataforma del cliente: ${req.headers["sec-ch-ua-platform"]}`);
-  // Respondemos
-  res.write("Esta es la respuesta del servidor.");
+
+  // Establecer el tipo de contenido que se entregara al cliente
+  res.setHeader("Content-Type", "text/html");
+
+  // Envio el contenido
+  res.write("<html>");
+  res.write("<head><title>My App</title></head>");
+  res.write(`<body><h1>Hello from the server &#128519;</h1><p style="color:red">Recurso solicitado: ${req.url}</p></body>`);
+  res.write("</html>");
+
   // Terminar la conexion
   res.end();
 });
