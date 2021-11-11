3f3229a7a954254b84932b6f66b185ba7400f95d
rivalcoba <ivan.rivalcoba@gmail.com>
2021-11-11 08:51:55 -0600

    S25 Procesando Formulario

--- index.js
+++ index.js
@@ -16,50 +16,98 @@ const server = http.createServer((req, res) => {
 
   // Informa en la consola del servidor que se recibe una petición
   console.log(`📮 Se ha solicitado el siguiente recurso: ${method}: ${url}`);
-  
-  // filtrar la url
-  if(url === '/'){
-    // Respuesta ante "Get /"
-    // 1. Estableciendo el tipo de retorno
-    // como HTML
-    res.setHeader('Content-Type', 'text/html');
-    // 2. Escribiendo la respuesta
-    res.write('<html>');
-    res.write('<head><title>My App</title></head>');
-    res.write('<body><h1>&#9889; Hello from my server &#9889;</h1></body>');
-    res.write('</html>');
-    // Cerrando conexion
-    res.end();
 
-  }else if(url === '/author'){
+  // filtrar la url
+  if (url === "/") {
     // Respuesta ante "Get /"
     // 1. Estableciendo el tipo de retorno
     // como HTML
-    res.setHeader('Content-Type', 'text/html');
-    let url_image = 'https://yt3.ggpht.com/ytc/AKedOLR8TH6qHa2gwbGT6muhZKx_IotBWygSNcG3LGzFosM=s900-c-k-c0x00ffffff-no-rj';
+    res.setHeader("Content-Type", "text/html");
     // 2. Escribiendo la respuesta
-    res.write('<html>');
-    res.write('<head><title>My App</title></head>');
-    res.write('<body>');
-    res.write('<h1>&#9889; Author &#9889;</h1>');
-    res.write('<p>Ivan Rivalcoba Rivas - Web Developer</p>');
-    res.write(`<img width="300px" src="${url_image}" alt="Foto Ivan Rivalcoba">`);
-    res.write('</body>');
-    res.write('</html>');
+    res.write(`
+    <html>
+      <head>
+        <title>Enter message</title>
+      </head>
+      <body>
+        <h1>Send Message</h1>
+        <form action="/message" method="POST">
+          <input type="text" name="message">
+          <button type="submit">send</button>
+        </form>
+      </body>
+    </html>
+    `);
     // Cerrando conexion
     res.end();
-  }else{
+  } else if (url === "/message" && method === "POST") {
+    // 1. Se crea una variable para guardar los datos de entrada
+    let body = [];
+    // 2. Registrar un manejador para la entrada de los datos
+    req.on("data", (chunk) => {
+      // 2.1 Registrando los trozos que llegan al backend
+      console.log(chunk);
+      // 2.2 Acumulo los datos de entrad
+      body.push(chunk);
+      // 2.3 Proteccion en caso de recepción masiva de datos
+      if (body.length > 1e6) req.socket.destroy();
+    });
+    // 3. Registrando un manejador de fin de recepción de datos
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      // 1. Estableciendo el tipo de retorno
+      // como HTML
+      res.setHeader("Content-Type", "text/html");
+      res.write(`
+      <html>
+      <head>
+        <title>Received Message</title>
+      </head>
+      <body>
+        <h1>Received Message</h1>
+        <p>Thank you!!!</p>
+        <p>The message we recieved was this: ${message}</p>
+      </body>
+    </html>
+      `);
+      // Finalizo conección
+      return res.end();
+    });
+  } else if (url === "/author") {
+    // Respuesta ante "Get /"
+    // 1. Estableciendo el tipo de retorno
+    // como HTML
+    res.setHeader("Content-Type", "text/html");
+    let url_image =
+      "https://yt3.ggpht.com/ytc/AKedOLR8TH6qHa2gwbGT6muhZKx_IotBWygSNcG3LGzFosM=s900-c-k-c0x00ffffff-no-rj";
+    // 2. Escribiendo la respuesta
+    res.write("<html>");
+    res.write("<head><title>My App</title></head>");
+    res.write("<body>");
+    res.write("<h1>&#9889; Author &#9889;</h1>");
+    res.write("<p>Ivan Rivalcoba Rivas - Web Developer</p>");
+    res.write(
+      `<img width="300px" src="${url_image}" alt="Foto Ivan Rivalcoba">`
+    );
+    res.write("</body>");
+    res.write("</html>");
+    // Cerrando conexion
+    res.end();
+  } else {
     // Se registra el Recurso no encontrado
     console.log(`❌ No se ha econtrado el recurso: ${url}`);
     // Recurso no econtrado
     // 1. Estableciendo el tipo de retorno
     // como HTML
-    res.setHeader('Content-Type', 'text/html');
+    res.setHeader("Content-Type", "text/html");
     // 2. Escribiendo la respuesta
-    res.write('<html>');
-    res.write('<head><title>My App</title></head>');
-    res.write('<body><h1>Error: 404 - Recurso no encontrado &#9940;</h1></body>');
-    res.write('</html>');
+    res.write("<html>");
+    res.write("<head><title>My App</title></head>");
+    res.write(
+      "<body><h1>Error: 404 - Recurso no encontrado &#9940;</h1></body>"
+    );
+    res.write("</html>");
     // Cerrando conexion
     res.end();
   }
