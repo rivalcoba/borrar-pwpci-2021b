a27337950de317474340e59d069cbbda0293e1d9
rivalcoba <ivan.rivalcoba@gmail.com>
2021-11-19 09:54:25 -0600

    S29 Instalación de express

--- index.js
+++ index.js
@@ -2,17 +2,21 @@
 import http from "http";
 // 2. Importando el module de routes
 import routes from "./routes.js"
+// 3. Importando express
+// $ npm i express -S
+import Express from 'express'
+
+// Crear una instancia de Express
+const app = Express();  // (req, res, next)=>{} event handler
 
 /**
  * Codigos de Emojies
  * Ref: https://www.w3schools.com/charsets/ref_emoji.asp
  */
 
-// 2. Crear el servidor
-// cb (callback) es una *funcion* que se ejecutara
-// ante cualquier peticion de un recurso a nuestro server
-// (request, response)
-const server = http.createServer(routes.requestHandler);
+// 2. Crear el servidor tomando como
+// manejador de peticiones a express
+const server = http.createServer(app);
 
 // 3. Pongo a trabajar el servidor
 // le paso un callback que escribira en la consola
